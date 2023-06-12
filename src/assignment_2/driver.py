bdfccb6f89045a230d56313392bfac5e0f2a2d65
manohartanna137 <tanna.manohar@diggibyte.com>
2023-06-12 12:45:58 +0530

    This is a driver file containing codes related to assignment 2

--- src/assignment_2/driver.py
+++ src/assignment_2/driver.py
@@ -6,22 +6,26 @@ add_columns,select_nested_columns,change_column,change_datatype,\
 spark = spark_session()
 
 df=create_dataframe(spark)
-df.show()
-
-df_select=select_nested_columns(df)
-df_select.show()
-
-df_add_col=add_columns(df)
-df_add_col.show()
-
-df_change_col=change_column(df)
-df_change_col.show()
-
-df_change_dt=change_datatype(df)
-df_change_dt.show()
-
-df_drop=drop_columns(df)
-df_drop.show()
+# df.show(truncate=False)
+#
+# df_select=select_nested_columns(df)
+# df_select.show(truncate=False)
+#
+# df_add_col=add_columns(df)
+# df_add_col.show(truncate=False)
+#
+#
+# df_change_col=change_column(df)
+# df_change_col.show(truncate=False)
+#
+# df_change_dt=change_datatype(df)
+# df_change_dt.show(truncate=False)
+#
+# df_nested_name=rename_nested_column(df)
+# df_nested_name.show(truncate=False)
+#
+# df_drop=drop_columns(df)
+# df_drop.show(truncate=False)
 
 df_distinct=distinct_value(df)
-df_distinct.show()
+df_distinct.show(truncate=False)
